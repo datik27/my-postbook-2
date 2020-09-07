80f578542ea423e7bcfdb09c89f170491dda9ecd
datik27 <itexpert508@gmail.com>
2020-09-07 23:55:28 +0800

    done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav className="mein_menu">
+        <ul>
+            <li><a href="/home">HOME</a></li>
+            <li><a href="/post">POSTS</a></li>
+            <li><a href="/blog">BLOG</a></li>
+        </ul>
+    </nav>
+  );
+};
+
+export default Header;
